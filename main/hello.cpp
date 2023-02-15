f11a5eb6889c31551fc1046079a8a90e5c66b616
namskash <namskash@gmail.com>
2023-02-15 11:03:34 +0530

    Changed hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello world\n";
+  cout << "This is PES1UG20CS260\n";
+  cout << "Hello Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
