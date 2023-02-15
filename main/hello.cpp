6c9c42f7f421cd92caf2d3f14608e43ba37f7e55
namskash <namskash@gmail.com>
2023-02-15 17:14:28 +0530

    Changes made

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello world\n";
-  cout << "This is PES1UG20CS260\n";
-  cout << "Hello Jenkins\n";
-  cout << "I have successfully built and run\n";
-  cout << "I have made changes to my hello.cpp file!\n";
-  return 0;
+	cout << "Hello world\n";
+	cout << "This is PES1UG20CS260\n";
+	cout << "Hello Jenkins\n";
+	cout << "I have successfully built and run\n";
+	cout << "I have made changes to my hello.cpp file!\n";
+	return 0;
 }
